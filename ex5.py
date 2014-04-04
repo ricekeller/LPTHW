5e4ecf3b4427685901bc0abc2d12e402f84ed91f
ricekeller <ricekeller@gmail.com>
2014-04-04 15:12:10 -0500

    5
    5

---
+++ ex5.py
@@ -0,0 +1,15 @@
+myname='ZZ ed'
+myage=12
+myheight=89
+myweight=111
+myeyes='red'
+myteeth='white'
+myhair='black'
+
+print("Let's talk about %s." % myname)
+print("He's %d inches tall." % myheight)
+print("He's %d pounds heavy." % myweight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair." %(myeyes,myhair))
+print("His teeth are usually %s depending on the coffee." % myteeth)
+print("If I add %d, %d, and %d I get %d." %(myage,myheight,myweight,myage+myheight+myweight))
