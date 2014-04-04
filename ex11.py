0e2c513700e745506c38173e615fc0f6c129ce7c
ricekeller <ricekeller@gmail.com>
2014-04-04 15:49:14 -0500

    11 13
    11 13

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?"),
+age=input("lalala:")
+print("How tall are you?"),
+height=input()
+print("How much do you weigh?"),
+weight=input()
+
+print("So, you are %r old, %r tall and %r heavy." % (age,height,weight))
